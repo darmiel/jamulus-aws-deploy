bf7d3c4e6b308371fddedd53638fef9f418903f3
darmiel <71837281+darmiel@users.noreply.github.com>
2021-05-19 18:59:38 +0200

    Implemented instance start, stop, terminate

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/darmiel/jamulus-aws-deploy
 
 go 1.16
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.2.12 // indirect
+	github.com/aws/aws-sdk-go v1.38.42 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,161 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"time"
+)
 
+const (
+	Region = "eu-central-1"
+)
+
+func GetPrettyState(state *ec2.InstanceState) string {
+	switch *state.Name {
+	case "pending":
+		return "⏱ pending"
+	case "running":
+		return "✅ running"
+	case "shutting-down":
+		return "🔻 shutting down"
+	case "terminated":
+		return "🗑 terminated"
+	case "stopping":
+		return "🥱 stopping"
+	case "stopped":
+		return "😴 stopped"
+	}
+	return ""
+}
+
+func main() {
+	// create session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(Region),
+	})
+	if err != nil {
+		log.Fatalln("Error creating session:", err)
+		return
+	}
+
+	ec := ec2.New(sess, aws.NewConfig().WithRegion(Region))
+	PrintMenu(ec)
+}
+
+func PrintMenu(ec *ec2.EC2) {
+	// get instances
+	instances, err := ec.DescribeInstances(&ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("tag-key"),
+				Values: []*string{aws.String("Jamulus")},
+			},
+			{
+				Name:   aws.String("instance-state-name"),
+				Values: aws.StringSlice([]string{"pending", "running", "shutting-down", "stopping", "stopped"}),
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	opt := make(map[string]*ec2.Instance)
+	for _, r := range instances.Reservations {
+		fmt.Println("*", *r.ReservationId)
+		for _, i := range r.Instances {
+			opt[fmt.Sprintf("[%s] %s [running for %s]", GetPrettyState(i.State), *i.InstanceId, time.Now().Sub(*i.LaunchTime).String())] = i
+		}
+	}
+
+	opts := make([]string, len(opt)+1)
+	i := 0
+	opts[i] = "♻️ Refresh"
+	for id, _ := range opt {
+		i++
+		opts[i] = id
+	}
+
+	// ask what to do
+	q := &survey.Select{
+		Message: "Select Instance",
+		Options: opts,
+	}
+
+	var instanceId string
+	if err := survey.AskOne(q, &instanceId); err != nil {
+		log.Fatalln("Error selecting instance:", err)
+		return
+	}
+
+	// (re-) print
+	if instanceId == "♻️ Refresh" {
+		PrintMenu(ec)
+		return
+	}
+
+	instance, ok := opt[instanceId]
+	if !ok {
+		log.Fatalln("Invalid instance selected")
+		return
+	}
+
+	// what to do?
+	q = &survey.Select{
+		Message: "Select Action",
+		Options: []string{"👋 Go Back", "Terminate Instance", "Stop Instance", "Start Instance"},
+	}
+
+	var action string
+	if err := survey.AskOne(q, &action); err != nil {
+		log.Fatalln("Error selecting action:", err)
+		return
+	}
+
+	switch action {
+	case "Terminate Instance":
+		log.Println("Terminating", *instance.InstanceId, "...")
+		resp, err := ec.TerminateInstances(&ec2.TerminateInstancesInput{
+			DryRun:      aws.Bool(false),
+			InstanceIds: []*string{instance.InstanceId},
+		})
+		if err != nil {
+			log.Fatalln("Error terminating instance:", err)
+			return
+		}
+		log.Println("Done! (", resp.String(), ")")
+		break
+
+	case "Stop Instance":
+		log.Println("Stopping", *instance.InstanceId, "...")
+		resp, err := ec.StopInstances(&ec2.StopInstancesInput{
+			InstanceIds: aws.StringSlice([]string{*instance.InstanceId}),
+		})
+		if err != nil {
+			log.Fatalln("Error stopping instance:", err)
+			return
+		}
+		log.Println("Done! (", resp.String(), ")")
+		break
+
+	case "Start Instance":
+		log.Println("Starting", *instance.InstanceId, "...")
+		resp, err := ec.StartInstances(&ec2.StartInstancesInput{
+			InstanceIds: aws.StringSlice([]string{*instance.InstanceId}),
+		})
+		if err != nil {
+			log.Fatalln("Error starting instance:", err)
+			return
+		}
+		log.Println("Done! (", resp.String(), ")")
+		break
+	case "👋 Go Back":
+		break
+	}
+	PrintMenu(ec)
 }
