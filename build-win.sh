57c4e5ee6742ef2273b6ebcbccb6b4d83a92a15b
darmiel <71837281+darmiel@users.noreply.github.com>
2021-06-11 16:49:58 +0200

    Implement owner in windows build script

--- build-win.sh
+++ build-win.sh
@@ -6,6 +6,6 @@ OUT_FILE="jaws-$(echo "${OWNER}" | tr '[:upper:]' '[:lower:]').exe"
 
 GOOS=windows GOARCH=${ARCH} \
   go build \
-  -ldflags "-X github.com/darmiel/jamulus-aws-deploy/internal/thin/common.Owner=Theo" \
+  -ldflags "-X github.com/darmiel/jamulus-aws-deploy/internal/thin/common.Owner=${OWNER}" \
   -o "${OUT_FILE}" \
   main.go
