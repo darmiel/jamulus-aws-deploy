beb562b9bb6b237c2e098018fec76f6fb50d46af
darmiel <71837281+darmiel@users.noreply.github.com>
2021-06-11 16:40:36 +0200

    Fixed windows build / owner

--- build-win.sh
+++ build-win.sh
@@ -6,6 +6,6 @@ OUT_FILE="jaws-$(echo "${OWNER}" | tr '[:upper:]' '[:lower:]').exe"
 
 GOOS=windows GOARCH=${ARCH} \
   go build \
-  -ldflags "-X github.com/darmiel/jamulus-aws-deploy/internal/thin/tsess.Owner=Theo" \
+  -ldflags "-X github.com/darmiel/jamulus-aws-deploy/internal/thin/common.Owner=Theo" \
   -o "${OUT_FILE}" \
   main.go
